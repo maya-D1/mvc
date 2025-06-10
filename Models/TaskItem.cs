a87d4f3b22670f0bcd9a0a16cb79c23896d97b5b
maya-D1 <maya.1dakkak@gmail.com>
2025-06-10 23:39:49 +0300

    project after editing

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -1,12 +1,12 @@
 namespace MvcTodoApp.Models
 {
     /// <summary>
-    /// يمثل نموذج مهمة في التطبيق.
+    ///يمثل نموذج مهمة في التطبيق
     /// </summary>
     public class TaskItem
     {
-        public int Id { get; set; }
-        public required string Title { get; set; }
-        public bool IsComplete { get; set; }
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public bool IsComplete { get; set; }
     }
 }
